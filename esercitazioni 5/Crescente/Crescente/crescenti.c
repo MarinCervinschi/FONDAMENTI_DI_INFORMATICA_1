21c3911859994297ea9e2b42a3151877b507c3cc
MarinCervinschi <marincervinschi3@gmail.com>
2024-02-21 15:11:21 +0100

    head

--- esercitazioni 5/Crescente/Crescente/crescenti.c
+++ esercitazioni 5/Crescente/Crescente/crescenti.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 bool crescente(unsigned int x){
- 
-    unsigned int x1 = x;
-    size_t num_valori = 0;
-    for (size_t i = 0; x1 * 0.5 != 0; ++i) {
-        x1 /= 10;
-        ++num_valori;
-    }
-    if (num_valori == 1) {
-        return true;
-    }
-    for (size_t i = 0; i != num_valori; ++i) {
-        unsigned int x2 = x;
-        x2 %= 10;
+    unsigned int y = x % 10;
+    x /= 10;
+    while (x != 0) {
+        if (y < x % 10) {
+            return false;
+        }
+        y = x % 10;
         x /= 10;
-        if(x == 0){return true;}
-        if(x2- 1 == x % 10 ){continue;}
-        else{return false;}
     }
     return true;
 }
 
-int main(void) {
- 
-    unsigned int x;
-    x = 56789;
-    unsigned int res;
-    res = crescente(x);
-    
-    return 0;
-}
+//int main(void) {
+//    if (crescente(12345)) {
+//        printf("Il numero è crescente!\n");
+//    }else {
+//        printf("Il numero NON è crescente!\n");
+//    }
+//    return 0;
+//}
